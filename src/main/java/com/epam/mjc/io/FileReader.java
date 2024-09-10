1f98b8e9b4f16780cf1fb84a451309cd1c060139
Zhabonchik <shapelnik@mail.ru>
2024-09-10 12:15:58 +0300

    First try

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileInputStream;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        StringBuilder data = new StringBuilder();
+        try(FileInputStream fileInputStream = new FileInputStream(file)){
+            int c;
+            while ((c = fileInputStream.read()) !=- 1){
+                data.append((char) c);
+            }
+            System.out.println(data);
+            String[] values = data.toString().split("\\s|\\n");
+            profile.setName(values[1]);
+            profile.setAge(Integer.parseInt(values[3]));
+            profile.setEmail(values[5]);
+            profile.setPhone(Long.parseLong(values[7]));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e){
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+        return profile;
     }
 }
